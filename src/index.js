307b891dc1c26ad7cc9e2d86ef13d267e1a8b0fc
Yu <ygao0018@gmail.com>
2020-05-22 10:33:09 +1000

    New changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-	return <div>Hi, there!</div>;
+	return (
+		<div className='ui container comments'>
+			<ApprovalCard>
+				<CommentDetail
+					author='Sam'
+					timeAgo='Today at 4:45PM'
+					content='Nice blog post'
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author='Alex'
+					timeAgo='Today at 2:00PM'
+					content='I like the subject'
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author='Jane'
+					timeAgo='Yesterday at 2:00PM'
+					content='I like writing'
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
